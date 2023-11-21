410da9dc3f4ba148e7839fdaf76c98f53c146e62
TonichaB <>
2023-11-21 21:37:59 +0000

    Feat: add introduction message to be presented to the user

--- run.py
+++ run.py
@@ -1,13 +1,36 @@
 import gspread
 import google.oauth2.service_account import Credentials
 
+# List of API's requiring access
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
+# Constant variables
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('habit_tracker')
+
+print('''
+╔─────────────────────────────────────────────────────────╗
+│██╗  ██╗ █████╗ ██████╗ ██╗████████╗                     │
+│██║  ██║██╔══██╗██╔══██╗██║╚══██╔══╝                     │
+│███████║███████║██████╔╝██║   ██║                        │
+│██╔══██║██╔══██║██╔══██╗██║   ██║                        │
+│██║  ██║██║  ██║██████╔╝██║   ██║                        │
+│╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝   ╚═╝                        │
+│                                                         │
+│████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗ │
+│╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗│
+│   ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝│
+│   ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗│
+│   ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║│
+│   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝│
+╚─────────────────────────────────────────────────────────╝
+''')
+print("Please press enter to start...")
+
+
