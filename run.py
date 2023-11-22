06420afa9f8a442f8642d43dafc62a64a90afc0c
TonichaB <>
2023-11-22 21:48:27 +0000

    Feat: add initial function for the starting page to show log in/register options

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
-import google.oauth2.service_account import Credentials
+from google.oauth2.service_account import Credentials
+import keyboard
 
 # List of API's requiring access
 SCOPE = [
@@ -14,23 +15,49 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('habit_tracker')
 
-print('''
-╔─────────────────────────────────────────────────────────╗
-│██╗  ██╗ █████╗ ██████╗ ██╗████████╗                     │
-│██║  ██║██╔══██╗██╔══██╗██║╚══██╔══╝                     │
-│███████║███████║██████╔╝██║   ██║                        │
-│██╔══██║██╔══██║██╔══██╗██║   ██║                        │
-│██║  ██║██║  ██║██████╔╝██║   ██║                        │
-│╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝   ╚═╝                        │
-│                                                         │
-│████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗ │
-│╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗│
-│   ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝│
-│   ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗│
-│   ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║│
-│   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝│
-╚─────────────────────────────────────────────────────────╝
+# Start Screen
+print(r '''
+  _   _   _   _   _     _   _   _   _   _   _   _  
+ / \ / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ 
+( H | a | b | i | t ) ( T | r | a | c | k | e | r )
+ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ 
 ''')
 print("Please press enter to start...")
 
 
+def login():
+    # log in function will go here
+    print("You have selected Log In")
+
+
+def register():
+    # register function will go here
+    print("You have selected Register")
+
+
+def startup():
+    # the following code has been created with the assistance of open.ai
+    # this code lets the user select login/register using arrow keys
+    options = ["Login", "Register"]
+    selected_option_index = 0
+
+    while True:
+        print("\nPlease select an option:")
+        for i, option in enumerate(options):
+            print(f"{'>> ' if i == selected_option_index else '   '}{option}")
+
+        key = keyboard.read_event(suppress=True).name
+
+        if key == "down" and selected_option_index < len(options) - 1:
+            selected_option_index += 1
+        elif key == "up" and selected_option_index > 0:
+            selected_option_index -= 1
+        elif key == "enter":
+            if options[selected_option_index] == "Login":
+                login()
+            elif options[selected_option_index] == "Register":
+                register()
+
+
+if __name__ == "__main__":
+    startup()
